f3a0886d0e8a317cbd123b27766fa90c7e412bd2
frenchkebab <tubowangin@gmail.com>
2022-07-20 23:32:49 +0900

    finished verification boooooyaaaaaaahhhhh

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require('@nomicfoundation/hardhat-toolbox');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.7',
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || '',
+      accounts:
+        process.env.RINKEBY_PRIVATE_KEY !== undefined
+          ? [process.env.RINKEBY_PRIVATE_KEY]
+          : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API,
+  },
 };
